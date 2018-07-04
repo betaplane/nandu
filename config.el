ff0cf79d96af9210444f59b1ba68ce9cb5758a5b
Arno C Hammann <5564291+betaplane@users.noreply.github.com>
2018-07-04 19:48:16 -0400

    now with additional pdf export and jupyter-notebook-like S-<return> YASnippet

--- config.el
+++ config.el
@@ -12,20 +12,7 @@
   ;;  (setq org-todo-keywords '((sequence "TODO" "|" "DONE")))
   )
 
-;; https://github.com/gregsexton/ob-ipython
-;; https://vxlabs.com/tag/ob-ipython/
-
-;; this is because ob-ipython uses python-shell-interpreter in order to
-;; find the "python" command, whereas for elpy I need to set it to "jupyter"
-;; ob-ipython currently uses this only in this particular function
-;; one might have to change it if that changes
-(with-eval-after-load 'ob-ipython
-	(defun ob-ipython--get-python ()
-      "python3"
-      ))
-
 (with-eval-after-load 'company
-  (global-company-mode t)
 	(add-to-list 'company-backends 'company-ob-ipython)
   )
 
@@ -36,17 +23,25 @@
   (setq yas-snippet-dirs `("~/Dropbox/spacemacs/snippets"
                            ,(concat (file-name-directory (locate-library "elpy")) "snippets"))))
 
-(with-eval-after-load 'yasnippet
-  (yas-global-mode 1))
-
 (setq python-shell-interpreter "jupyter-console")
 
 ;; don't prompt me to confirm everytime I want to evaluate a block
+;; this is a possible security risk if I open an org-mode file from someone else
+;; and the code gets exevuted automatically
 (setq org-confirm-babel-evaluate nil)
+;; (setq org-confirm-shell-link-function nil) ; doesn't seem to apply
 
 ;; display/update images in the buffer after I evaluate
-(add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
+(add-hook 'org-babel-after-execute-hook 'nandu-org-after-execute-hook)
+
+(add-hook 'org-mode-hook 'nandu-set-ob-ipython-directory)
+
+(add-hook 'after-init-hook (lambda ()
+                             (yas-global-mode t)
+                             (global-company-mode t)))
 
 ;; emacs isn't really executing any of the shell init files
 ;; I need this for some of my python code currently
-(setenv "CEZANNE_CONFIG" (shell-command-to-string "echo $CEZANNE_CONFIG"))
+(setenv "CEZANNE_CONFIG" "~/Dropbox/general.cfg")
+
+(defvar nandu-shift-return nil)
--- funcs.el
+++ funcs.el
@@ -1,17 +1,94 @@
 (defun switch-pyvenv (env)
   (interactive "spyvenv: ")
-  (setq lenv (split-string env))
-  (setq conda (pop lenv))
-  (if (equal conda "conda")
+  (let ((lenv (split-string env)))
+    (if (equal (car lenv) "conda")
+        (let ((env (pop (cdr lenv))))
+          (pyvenv-activate (expand-file-name env conda-envs-directory))
+          (setq-local python-shell-interpreter-args
+                      (concat "--simple-prompt --kernel " env))
+          )
       (progn
-        (setq pyvenv-base-dir conda-envs-directory)
-        (setq env (pop lenv))
-        (setq pscomm "--simple-prompt --kernel "))
-    (progn
-      (setq pyvenv-base-dir "/home/arno/Documents/code/")
-      (setq env conda)
-      (setq pscomm "--kernel ")))
-  (pyvenv-activate (concat pyvenv-base-dir env))
-  ;; (setenv "MPLBACKEND" "module://Qt5Agg")
-  (setq python-shell-interpreter-args (concat pscomm env))
+        (pyvenv-activate (expand-file-name env non-conda-envs-directory))
+        (setq-local python-shell-interpreter-args
+                    (concat "--kernel " env))
+        )))
   )
+
+;; https://github.com/gregsexton/ob-ipython
+;; https://vxlabs.com/tag/ob-ipython/
+
+(with-eval-after-load 'ob-ipython
+  ;; this is because ob-ipython uses python-shell-interpreter in order to
+  ;; find the "python" command, whereas for elpy I need to set it to "jupyter"
+  ;; ob-ipython currently uses this only in this particular function
+  ;; one might have to change it if that changes
+	(defun ob-ipython--get-python () "python3")
+
+  (defun nandu-set-ob-ipython-directory ()
+    (let ((encl (file-name-directory (buffer-file-name)))
+          (res_dir (car (get 'ob-ipython-resources-dir 'standard-value))))
+      (setq-local ob-ipython-resources-dir
+                  (file-name-as-directory
+                   (expand-file-name
+                    (file-name-base (buffer-file-name)) (concat encl res_dir))))))
+
+  ;; this overrides the rendering of output in ob-ipython,
+  ;; in particular to allow both png and pdf files to be saved,
+  ;; but only the png to be included in the org file
+  (defun ob-ipython--render (file-or-nil values)
+    (message "NANDUPY")
+    (let* ((res_dir (buffer-local-value 'ob-ipython-resources-dir (current-buffer)))
+          (file-base (or file-or-nil (make-temp-name res_dir)))
+          (org (lambda (value) value))
+          (pdf (lambda (value)
+                 (let ((file (s-concat file-base ".pdf")))
+                   (ob-ipython--write-base64-string file value))))
+          (png (lambda (value)
+                 (let ((file (s-concat file-base ".png")))
+                   (ob-ipython--write-base64-string file value)
+                   (format "[[file:%s]]" file))))
+          (svg (lambda (value)
+                 (let ((file (s-concat file-base ".svg")))
+                   (ob-ipython--write-string-to-file file value)
+                   (format "[[file:%s]]" file))))
+          (txt (lambda (value)
+                 (let ((lines (s-lines value)))
+                   (if (cdr lines)
+                       (->> lines
+                            (-map 's-trim)
+                            (s-join "\n  ")
+                            (s-concat "  ")
+                            (format "#+BEGIN_EXAMPLE\n%s\n#+END_EXAMPLE"))
+                     (s-concat ": " (car lines)))))))
+        (-when-let (val (cdr (assoc 'application/pdf values))) (funcall pdf val))
+        (or (-when-let (val (cdr (assoc 'text/org values))) (funcall org val))
+            (-when-let (val (cdr (assoc 'image/png values))) (funcall png val))
+            (-when-let (val (cdr (assoc 'image/svg+xml values))) (funcall svg val))
+            (-when-let (val (cdr (assoc 'text/plain values))) (funcall txt val)))))
+    )
+
+(defun nandu--ob-ipython-shift-return (&optional yas)
+  (when (or nandu-shift-return yas)
+    (let* ((pos (point))
+           (el1 (org-element-at-point))
+           (el2 nil))
+      (when (string= (car el1) "src-block")
+        (setq pos (plist-get (pop (cdr el1)) :end))
+        (save-excursion
+          (goto-char pos)
+          (setq el2 (org-element-at-point))
+          (when (string= (car el2) "drawer")
+            (setq pos (plist-get (pop (cdr el2)) :end)))))
+      (if (and (not (string= (car el2) "drawer")) (string= (car el1) "src-block"))
+          (progn
+            (setq-local nandu-shift-return t)
+            (org-babel-execute-src-block))
+        (progn
+          (goto-char pos)
+          (setq-local nandu-shift-return nil)
+          (yas-expand-snippet (yas-lookup-snippet "ob-ipython source block")))))))
+
+
+(defun nandu-org-after-execute-hook ()
+  (org-display-inline-images t t)
+  (nandu--ob-ipython-shift-return))
