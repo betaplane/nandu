b1a7c24db14a7ba03de160e4424cc2a24b3dcaac
Arno Hammann <5564291+betaplane@users.noreply.github.com>
2018-09-14 14:22:12 -0300

    minor updates

--- pandas2org.py
+++ pandas2org.py
@@ -21,7 +21,7 @@ def table(df):
     cols = index(df.columns)
     M = df.as_matrix()
     s = '|\n|'.join('|'.join(' ' for _ in range(len(idx[0]))) + '|' + \
-                          '|'.join(c[i] for c in cols) for i in range(len(cols[0]))) + \
+                          '|'.join(str(c[i]) for c in cols) for i in range(len(cols[0]))) + \
         '|\n|' + '|'.join('-' for _ in range(len(idx[0]) + len(M[0]))) + '|\n|' + \
         '|\n|'.join('|'.join(str(i) for j in z for i in j) for z in zip(idx, M))
     return publish_display_data({'text/org': '|' + s + '|'})
